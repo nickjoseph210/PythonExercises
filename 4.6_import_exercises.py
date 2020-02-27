520945332ba1ad4b729cd699ba3ffa036881cca3
Nick <nickpjoseph210@gmail.com>
2020-02-27 16:20:37 -0600

    Exercises on importing with Python

---
+++ 4.6_import_exercises.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from functions_exercises2 import is_two, is_vowel, is_consonant
+
+
+# In[ ]:
+
+
+#2a: Using itertools, there are three you can combine the letters from "abc" with the nubmers 1, 2, and 3 using 
+# combinations(), product(), and permutations()
+
+#2b: Using itertools, you can combine two of the letters from 'abcd' 4 ways: groupby(), combinations(),
+# permutations(), and grouper().
+
