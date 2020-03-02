6ffb296fc3b05cc72f0dbebb08de15195a691634
Nick <nickpjoseph210@gmail.com>
2020-03-02 06:12:06 -0600

    Matplotlib exercises complete before lecture

--- 4.8.2_Matplotlib_Exercises.py
+++ 4.8.2_Matplotlib_Exercises.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[8]:
+# In[4]:
 
 
 from matplotlib import pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[30]:
+# In[31]:
 
 
 #x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -19,22 +19,115 @@ y = [(i**2 - i) for i in x] # list comp for the maths required; y-axis
 
 plt.plot(x, y)
 
-plt.text(-4.5, 0, "Point of Origin (0,0)") # moved from (0,0 because it looked ugly)
-
+# plt.text(-4.5, 0, "Point of Origin (0,0)") # moved from (0,0 because it looked ugly)
+plt.annotate("Origin 0,0", xy=(0, 0), xytext=(-2.5, 7), arrowprops={'facecolor': 'red'})
 plt.show()
 
 
-# In[3]:
+# In[15]:
 
 
 # for y = square root of x
 x = [i for i in range (11)]
-y = [(i \sqrt{i}) for i in x]
+y = [i**0.5 for i in x]
 plt.plot(x, y)
 plt.xlabel('x-value')
-plt.ylabel('y = \sqrt{x}')
-plt.title('Chart 1: x v. y')
+plt.ylabel(r'$y = \sqrt{x}$')
+plt.title('Chart 1: Ex versus Why')
 plt.show()
+
+# for y = x^3
+x = [i for i in range(11)]
+y = [i**3 for i in x]
+plt.plot(x,y)
+plt.xlabel('x-value')
+plt.ylabel('y = x^3')
+plt.title('Chart 2: Why = Ex Cubed')
+plt.show()
+
+import math
+
+# for y = tan(x)
+x = [i for i in range(11)]
+y = [math.tan(i)for i in range(11)]
+
+plt.plot(x, y)
+plt.xlabel('x-value')
+plt.ylabel('$\ tan$')
+plt.title('Tangent of Ex')
+plt.show()
+
+# for y = 2^x
+x = [i for i in range(11)]
+y = [(2**i + 1) for i in x] 
+
+plt.plot(x, y)
+plt.xlabel('x-value')
+plt.ylabel('$2^x$')
+plt.title('Exponential Function')
+plt.show()
+
+
+# In[23]:
+
+
+import math
+
+# for y = square root of x
+
+all_x = [i for i in range (11)]
+y = [i**0.5 for i in x]
+plt.plot(x, y)
+
+# for y = x^3
+y = [i**3 for i in x]
+plt.plot(x,y)
+
+# for y = tan(x)
+y = [math.tan(i)for i in range(11)]
+plt.plot(x,y)
+
+# for y = 2^x
+y = [(2**i + 1) for i in x] 
+plt.plot(x,y)
+
+
+plt.xlabel('x-value')
+plt.ylabel('y-values')
+plt.title('Chart 5: Total Plots')
+
+plt.show()
+
+# # for y = x^3
+# x = [i for i in range(11)]
+# y = [i**3 for i in x]
+# plt.plot(x,y)
+# plt.xlabel('x-value')
+# plt.ylabel('y = x^3')
+# plt.title('Chart 2: Why = Ex Cubed')
+# plt.show()
+
+# import math
+
+# # for y = tan(x)
+# x = [i for i in range(11)]
+# y = [math.tan(i)for i in range(11)]
+
+# plt.plot(x, y)
+# plt.xlabel('x-value')
+# plt.ylabel('$\ tan$')
+# plt.title('Tangent of Ex')
+# plt.show()
+
+# # for y = 2^x
+# x = [i for i in range(11)]
+# y = [(2**i + 1) for i in x] 
+
+# plt.plot(x, y)
+# plt.xlabel('x-value')
+# plt.ylabel('$2^x$')
+# plt.title('Exponential Function')
+# plt.show()
 
 
 # In[ ]:
